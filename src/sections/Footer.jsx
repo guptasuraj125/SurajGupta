a56ecb7d1f1fb87bc7e8aebd383d84220ada7dad
Gupta Suraj Krishna <surajguptagit2005@gmail.com>
2025-02-02 17:10:42 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,24 +2,27 @@ const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
       <div className="text-white-500 flex gap-2">
-        <p>Terms & Conditions</p>
+        <p>Quick Links:</p>
         <p>|</p>
-        <p>Privacy Policy</p>
+        <a href="https://github.com/guptasuraj125" className="hover:underline">GitHub</a>
+        <p>|</p>
+        <a href="https://www.instagram.com/suraj.gupta2006/profilecard/?igsh=MWpuNjNibHR3Z2traw==" className="hover:underline">Instagram</a>
       </div>
 
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <a href="https://github.com/guptasuraj125" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          </a>
         </div>
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+          <a href="https://www.instagram.com/suraj.gupta2006/profilecard/?igsh=MWpuNjNibHR3Z2traw==" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+          </a>
         </div>
       </div>
 
-      <p className="text-white-500">© 2024 Adrian Hajdin. All rights reserved.</p>
+      <p className="text-white-500">© 2024 Gupta Suraj. All rights reserved.</p>
     </footer>
   );
 };
